9da2c8f0d610b00e0dcbbb2fdeebdbafcb53d9d0
Amelevi-dev <amelevigloriaamededzisso@gmail.com>
2026-01-07 10:47:36 +0000

    Premier envoi de mon projet

---
+++ index.js
@@ -0,0 +1,10 @@
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+document.addEventListener('DOMContentLoaded', function () {
+	AOS.init({
+		once: true,
+		duration: 800,
+		easing: 'ease-in-out'
+	});
+});
